ef4e20ed521d3a3c566f7d97b0aaf5604fbf86e3
KSauermannWSEI <krzysztof.sauermann@outlook.com>
2019-02-07 18:42:16 +0100

    następny przykład

--- Indeksery/Program.cs
+++ Indeksery/Program.cs
@@ -10,15 +10,23 @@ namespace Indeksery
     {
         static void Main(string[] args)
         {
-            Names names = new Names(5);
-            names[0] = "Roman";
-            names[1] = "Aśka";
-            names[2] = "Krzyś";
+            //Names names = new Names(5);
+            //names[0] = "Roman";
+            //names[1] = "Aśka";
+            //names[2] = "Krzyś";
 
-            for (int i = 0; i < names.Count; i++)
-            {
-                Console.WriteLine(names[i]);
-            }
+            //for (int i = 0; i < names.Count; i++)            
+            //    Console.WriteLine(names[i]);
+            
+            Years years = new Years();
+            years["Roman"] = 1900;
+            years["Aśka"] = 1950;
+            years["Krzyś"] = 1990;
+
+            Console.WriteLine(years["Roman"]);
+            Console.WriteLine(years["Aśka"]);
+            Console.WriteLine(years["Krzyś"]);
+
             Console.Read();
         }
     }
---
+++ IndexersClasses/Years.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexersClasses
+{
+    public class Years
+    {
+        private Dictionary<string, int> years;
+        public int Count => years.Count;
+        public Years()
+        {
+            years = new Dictionary<string, int>();
+        }
+
+        public int this[string index]
+        {
+            get
+            {
+                if (!years.ContainsKey(index))
+                    throw new IndexOutOfRangeException("Index nie istnieje");
+                return years[index];
+            }
+            set
+            {
+                if (!years.ContainsKey(index))
+                    years.Add(index, value);
+                else
+                    years[index] = value;
+            }
+        }
+    }
+}
